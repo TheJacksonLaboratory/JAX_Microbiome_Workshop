af413112b29d93dd7b895aa0f09c9e97711ce3a4
snamburi3 <sandeep.namburi@jax.org>
2020-10-26 18:21:50 -0400

    add scripts

--- scripts/create-table.py
+++ scripts/create-table.py
@@ -9,13 +9,15 @@ inputs=sys.argv[1]
 output=sys.argv[2]
 #names=["Username", "IP", "Terminal 1", "Terminal 2", "RStudio", "Jupyter", "Download Files"]
 df = pd.read_csv(inputs, sep=",", header=None, names=["Username", "IP"])
-df['Terminal 1'] = df["IP"].map(lambda beta_value: "<a href='http://{}:8080' target='_blank'>terminal 1</a>".format(beta_value))
-df['Terminal 2'] = df["IP"].map(lambda beta_value: "<a href='http://{}:8080' target='_blank'>terminal 2</a>".format(beta_value))
-df['RStudio'] = df["IP"].map(lambda beta_value: "<a href='http://{}:8787' target='_blank'>rstudio</a>".format(beta_value))
-df['Jupyter'] = df["IP"].map(lambda beta_value: "<a href='http://{}:8888' target='_blank'>jupyter</a>".format(beta_value))
-df['Download Files'] = df["IP"].map(lambda beta_value: "<a href='http://{}' target='_blank'>download files</a>".format(beta_value))
+df['Terminal 1'] = df["IP"].map(lambda beta_value: "<a href='{}/terminal/' target='_blank'>terminal 1</a>".format(beta_value))
+df['Terminal 2'] = df["IP"].map(lambda beta_value: "<a href='{}/terminal2/' target='_blank'>terminal 2</a>".format(beta_value))
+df['RStudio'] = df["IP"].map(lambda beta_value: "<a href='{}/rstudio' target='_blank'>rstudio</a>".format(beta_value))
+df['Download Files'] = df["IP"].map(lambda beta_value: "<a href='{}' target='_blank'>download files</a>".format(beta_value))
 
-df.to_csv("csv-intermediate-file-csv", index=False, header=["Username", "IP", "Terminal 1", "Terminal 2", "RStudio", "Jupyter", "Download Files"])
+print(df)
+print(df.columns)
+del df['IP']
+df.to_csv("csv-intermediate-file-csv", index=False, header=["User", "Terminal 1", "Terminal 2", "RStudio", "Download Files"])
 
 def csvtomd(output):
     return subprocess.Popen(
--- scripts/modify-instance-atribute.sh
+++ scripts/modify-instance-atribute.sh
@@ -2,6 +2,4 @@
 
 aws ec2 describe-instances --query 'Reservations[?Instances[?Tags[?Key==`microbiome2020-prod`]]].Instances[].InstanceId' --profile microbiome | grep i- \
  | awk '{print $1}' \
- | sed 's/\"//g' \
- | sed 's/,//' \
- | xargs -L1 -I% aws ec2 describe-instances --instance-ids % --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text --profile microbiome
+ | sed 's/\"//g' | sed 's/,//' | xargs -L1 -I% aws ec2 modify-instance-attribute --instance-id  % --no-disable-api-termination --profile microbiome  
